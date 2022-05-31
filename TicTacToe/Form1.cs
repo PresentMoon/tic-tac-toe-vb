cd3f0eaae3a23389f99f4ab55c2b0352e4e120d1
PresentMoon <irishmiguelpulido@gmail.com>
2022-06-01 03:47:14 +0800

    Win Conditions

--- TicTacToe/Form1.cs
+++ TicTacToe/Form1.cs
@@ -12,59 +12,105 @@ namespace TicTacToe
 {
     public partial class Form1 : Form
     {
+
         public Form1()
         {
             InitializeComponent();
         }
 
-        private void Form1_Load(object sender, EventArgs e)
+        public int players = 2; //even = O move; odd = X turn
+        public int moves = 0; //counting moves;
+        //counting wins or draws of players 
+
+        private void buttonClick(object sender, EventArgs e)
         {
+            Button button = (Button)sender;
+            if (players % 2 != 0)
+            {
+                button.Text = "O";
+                players++;
+                moves++;
+            }
+            else
+            {
+                button.Text = "X";
+                players++;
+                moves++;
+            }
+            if (CheckDraw() == true)
+            {
+                MessageBox.Show("It's a Draw!");
+                NewGame();
+            }
+
+            if(CheckWinner()==true)
+            {
+                if (button.Text =="X")
+                {
+                    MessageBox.Show("X Wins!");
+                    NewGame();
+                }
+                else
+                {
+                    MessageBox.Show("O Wins!");
+                    NewGame();
+                }
+            }
 
         }
 
-        private void button1_Click(object sender, EventArgs e)
+        void NewGame()
         {
-
+            players = 2;
+            moves = 0;
+            A1.Text = A2.Text = A3.Text = B1.Text = B2.Text = B3.Text = C1.Text = C2.Text = C3.Text = "";
         }
 
-        private void button8_Click(object sender, EventArgs e)
+        private void NGbuttonClick(object sender, EventArgs e)
         {
-
+            NewGame();
         }
 
-        private void button7_Click(object sender, EventArgs e)
+        private void EButtonClick(object sender, EventArgs e)
         {
-
+            this.Close();
         }
 
-        private void button6_Click(object sender, EventArgs e)
+        bool CheckDraw()
         {
-
+            if (moves == 9)
+                return true;
+            else
+                return false;
         }
 
-        private void button5_Click(object sender, EventArgs e)
+        bool CheckWinner()
         {
+            //horizontal checks
+            if ((A1.Text == B1.Text) && (B1.Text == C1.Text) && A1.Text != "")
+                return true;
+            else if ((A2.Text == B2.Text) && (B2.Text == C2.Text) && A2.Text != "")
+                return true;
+            else if ((A3.Text == B3.Text) && (B3.Text == C3.Text) && A3.Text != "")
+                return true;
 
+            //vertical checks
+            if ((A1.Text == A2.Text) && (A2.Text == A3.Text) && A1.Text != "")
+                return true;
+            else if ((B1.Text == B2.Text) && (B2.Text == B3.Text) && B1.Text != "")
+                return true;
+            else if ((C1.Text == C2.Text) && (C2.Text == C3.Text) && C1.Text != "")
+                return true;
+
+            //diagonal checks
+            if ((A1.Text == B2.Text) && (B2.Text == C3.Text) && A1.Text != "")
+                return true;
+            else if ((C1.Text == B2.Text) && (B2.Text == A3.Text) && C1.Text != "")
+                return true;
+            else
+                return false;
         }
-
-        private void button4_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void button3_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void button2_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void button9_Click(object sender, EventArgs e)
-        {
 
         }
     }
-}
+
